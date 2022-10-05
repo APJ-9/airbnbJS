bfd1f568c780c1b8f763bc969d4429395c9c32d6
APJ-9 <akhil.jose@vonnue.com>
2022-10-05 17:32:55 +0530

    modified the script

--- script.js
+++ script.js
@@ -20,49 +20,77 @@ function dataConnection() {
     }
     xhr.send()
 }
-dataConnection()
+
 
 function createCard(element) {
-    console.log(element)
+
+    const allImages = element.images
+
     const card = document.createElement('div')
     card.classList.add('main-card')
-    card.innerHTML =
+
+    const slideShowContainer = document.createElement('div')
+    slideShowContainer.classList.add('slide-show')
+
+    const slideShow = document.createElement('div')
+    slideShow.classList.add('images')
+
+    const rightBtn = document.createElement('button')
+    rightBtn.classList.add('btn')
+    rightBtn.innerText = '>'
+
+    const leftBtn = document.createElement('button')
+    leftBtn.classList.add('btn')
+    leftBtn.innerText = '<'
+
+
+    rightBtn.addEventListener('click', () => {
+        console.log(element.images)
+    })
+
+    leftBtn.addEventListener('click', () => {
+        console.log(element.name)
+    })
+    const heart = document.createElement('div')
+    heart.classList.add('heart')
+    heart.innerHTML =
         `
-        <div class="slide-show">
-            <div class="image" id="images">
-                <img src="${element.img1}">
-                <img src="${element.img2}">
-                <img src="${element.img3}">
-                <img src="${element.img4}">
-                <img src="${element.img5}">
-                <img src="${element.img6}">
-                <img src="${element.img7}">
-                <img src="${element.img8}">
-                <img src="${element.img9}">
-                <img src="${element.img10}">
-            </div>
-            <div class="btn-containers">
-                <button id="left" class="btn"> < </button>
-                <button id="right" class="btn"> > </button>
-            </div>
-            <div class="heart">
-                <i class="fa fa-heart-o"></i>
-            </div>
-        </div>
-        <div class="main-card-elements">
-            <div class="name-and-rating">
-                <h3>${element.name}</h3>
-                <p><i class="fa fa-star"></i>&nbsp;${element.rating}</p>
-            </div>
-            <div class="other-details">
-                <p>${element.address} </p>
-                <p>${element.date}</p>
-                <div class="amount">
-                    <h3>&#8377;${element.price}</h3>
-                    <span>&nbsp;${element.day}
-                </div>
-            </div>
-        </div>
+        <i class="fa fa-heart-o"></i>
     `
+
+    const cardDetails = document.createElement('div')
+    cardDetails.classList.add('main-card-elements')
+    cardDetails.innerHTML = `
+    <div class="name-and-rating">
+        <h3>${element.name}</h3>
+        <p><i class="fa fa-star"></i>&nbsp;${element.rating}</p>
+    </div>
+    <div class="other-details">
+        <p>${element.address} </p>
+        <p>${element.date}</p>
+        <div class="amount">
+            <h3>&#8377;${element.price}</h3>
+            <span>&nbsp;${element.day}
+        div>
+    div>
+    `
+
+    card.appendChild(slideShowContainer)
+    slideShowContainer.append(heart)
+    slideShowContainer.appendChild(leftBtn)
+    slideShowContainer.appendChild(rightBtn)
+    slideShowContainer.appendChild(slideShow)
+    allImages.forEach(images => {
+        const img = document.createElement('img')
+        img.setAttribute('src', images)
+        slideShow.appendChild(img)
+    })
+
+    slideShowContainer.appendChild(cardDetails)
+
     mainContainer.appendChild(card)
+
 }
+
+
+dataConnection()
